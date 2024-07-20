7b001e719616fc6cd8acc4098038f37877c3badb
rex <81349047+cjsrkd3321@users.noreply.github.com>
2024-07-20 19:41:38 +0900

    docker compose up -d --build 붙이는 이유 보여주기 위한 명령어 추가

--- 기본학습/compose/build/build.sh
+++ 기본학습/compose/build/build.sh
@@ -3,6 +3,9 @@
 # compose.yaml 서비스에 build가 존재하는 모든 것들 빌드 진행
 docker compose build
 
+# 소스코드가 변경 되더라도 반영되지 않음.
+docker compose up -d
+
 # 항상 빌드 하면서 up 시키기
 # 컴포즈는 동일한 이미지 이름이 존재하는 경우, build를 생략함
 docker compose up -d --build
